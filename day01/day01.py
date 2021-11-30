be547439b10828452c062508f42bcbb9a59c713f
Meithan West <meithan@gmail.com>
2021-11-30 17:13:31 -0600

    Initial preparations

---
+++ day01/day01.py
@@ -0,0 +1,32 @@
+# Day 01
+
+import sys
+
+# ==============================================================================
+
+# If no specific input given, default to "day<X>.in"
+if len(sys.argv) == 1:
+  sys.argv.append(sys.argv[0].replace(".py", ".in"))
+
+# Read (and possibly pre-process) input
+with open(sys.argv[1]) as f:
+  for line in f:
+    pass
+
+# ------------------------------------------------------------------------------
+# Part 1
+
+
+
+ans1 = 0
+
+print("Part 1:", ans1)
+
+# ------------------------------------------------------------------------------
+# Part 2
+
+
+
+ans2 = 0
+
+print("Part 2:", ans2)
