b7a8a4e83fdf659c9009663f8a28dde6f6319e75
Meithan West <meithan@gmail.com>
2021-11-30 23:16:00 -0600

    Day 1 solution

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ I'll be updating this as a sort of mini blog whenever I can, commenting on the d
 
 You can also check out our fancy [custom private leaderboard](https://meithan.net/AoC21/), with medals awarded to the fastest solvers. See (and download/fork!) the project [here](https://github.com/meithan/AoCBoard).
 
-No solutions to show yet.
+Go to day: [1](#day1)
 
 ---
+
+**Day 1**: [Sonar Sweep](https://adventofcode.com/2020/day/1)<a name="day1"></a>
+
+3m 11s (#1552) / 6m 04s (#973) - [code](https://github.com/meithan/AoC20/blob/main/solutions/day01/day01.py)
+
+As usual Day 1 is just a warm-up. Not very impressed with the time it took me to correctly code a few lines of code, but oh well. At least I got gold stars on our [private leaderboard](https://meithan.net/AoC21/).
--- day01/day01.py
+++ day01/day01.py
@@ -1,4 +1,4 @@
-# Day 01
+# Day 1: Sonar Sweep
 
 import sys
 
@@ -9,24 +9,32 @@ if len(sys.argv) == 1:
   sys.argv.append(sys.argv[0].replace(".py", ".in"))
 
 # Read (and possibly pre-process) input
+depths = []
 with open(sys.argv[1]) as f:
   for line in f:
-    pass
+    depths.append(int(line.strip()))
 
 # ------------------------------------------------------------------------------
 # Part 1
 
+count = 0
+for i in range(1, len(depths)):
+ if depths[i] > depths[i-1]:
+   count +=1
 
-
-ans1 = 0
-
+ans1 = count
 print("Part 1:", ans1)
 
 # ------------------------------------------------------------------------------
 # Part 2
 
+count2 = 0
+last_sum = None
+for i in range(2,len(depths)):
+  cur_sum = depths[i] + depths[i-1] + depths[i-2]
+  if last_sum is not None and cur_sum > last_sum:
+    count2 += 1
+  last_sum = cur_sum
 
-
-ans2 = 0
-
+ans2 = count2
 print("Part 2:", ans2)
