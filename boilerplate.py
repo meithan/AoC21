dd2065b3afbd971141286b51315228e49e47892a
Meithan West <meithan@gmail.com>
2021-12-04 23:43:30 -0600

    Tweak boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -8,7 +8,7 @@ import sys
 if len(sys.argv) == 1:
   sys.argv.append(sys.argv[0].replace(".py", ".in"))
 
-# Read (and possibly pre-process) input
+# Parse input
 with open(sys.argv[1]) as f:
   for line in f:
     pass
@@ -18,15 +18,15 @@ with open(sys.argv[1]) as f:
 
 
 
-ans1 = 0
 
-print("Part 1:", ans1)
+
+print("Part 1:", 0)
 
 # ------------------------------------------------------------------------------
 # Part 2
 
 
 
-ans2 = 0
 
-print("Part 2:", ans2)
+
+print("Part 2:", 0)
