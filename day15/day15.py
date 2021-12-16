df750c6e36f71c2e9b3a876c356137c82dc8063e
Meithan West <meithan@gmail.com>
2021-12-15 19:20:48 -0600

    Day 15 solution

--- day15/day15.py
+++ day15/day15.py
@@ -1,6 +1,5 @@
-# Day XX
+# Day 15: Chiton
 
-from collections import defaultdict
 from queue import PriorityQueue
 import sys
 
@@ -19,7 +18,7 @@ with open(sys.argv[1]) as f:
 # ------------------------------------------------------------------------------
 
 # A* search
-def AStar(start, end, grid, NX, NY):
+def A_Star(start, end, grid, NX, NY):
 
   def neighbors(node):
     x, y = node
@@ -42,37 +41,31 @@ def AStar(start, end, grid, NX, NY):
     path = [node]
     cost = 0
     while node in predecessor:
-      cost += grid[node[0]][node[1]] 
+      cost += grid[node[0]][node[1]]
       node = predecessor[node]
       path.append(node)
     path.reverse()
     return path, cost
 
   queue = PriorityQueue()
-  openset = set()
+  queue.put((0, start))
   predecessor = {}
-  gscore = defaultdict(lambda: float("inf"))
-  gscore[start] = 0
-  fscore = defaultdict(lambda: float("inf"))
-  fscore[start] = h(start)
-  queue.put((fscore[start], start))
-  openset.add(start)
+  predecessor[start] = None
+  past_cost = {}
+  past_cost[start] = 0
 
   while not queue.empty():
-    f, current = queue.get()
+    _, current = queue.get()
     if current == end:
       return build_path(current)
-    openset.remove(current)
     for neigh in neighbors(current):
-      tent_gscore = gscore[current] + grid[neigh[0]][neigh[1]]
-      if tent_gscore < gscore[neigh]:
+      next_cost = past_cost[current] + grid[neigh[0]][neigh[1]]
+      if neigh not in past_cost or next_cost < past_cost[neigh]:
+        past_cost[neigh] = next_cost
         predecessor[neigh] = current
-        gscore[neigh] = tent_gscore
-        fscore[neigh] = tent_gscore + h(neigh)
-        if neigh not in openset:
-          openset.add(neigh)
-          queue.put((fscore[neigh], neigh))
-
+        priority = next_cost + h(neigh)
+        queue.put((priority, neigh))
+        
 # ------------------------------------------------------------------------------
 # Part 1
 
@@ -81,7 +74,7 @@ NY = len(grid)
 start = (0,0)
 end = (NX-1,NY-1)
 
-path, cost = AStar(start, end, grid, NX, NY)
+path, cost = A_Star(start, end, grid, NX, NY)
 
 print("Part 1:", cost)
 
@@ -106,6 +99,6 @@ for i in range(NX2):
 
 start = (0,0)
 end2 = (NX2-1,NY2-1)
-path2, cost2 = AStar(start, end2, grid2, NX2, NY2)
+path2, cost2 = A_Star(start, end2, grid2, NX2, NY2)
 
 print("Part 2:", cost2)
